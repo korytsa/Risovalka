00f644b9411f391d385e6d05d2bd5a031128ea44
user <anyakorytko@mail.ru>
2020-08-25 02:26:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const state = {
+    currentColor : '',
+    prevColor : 'pink',
+    currentTool : 'pencil',
+    
+}
+
+let current = document.querySelector('.current');
+let previous = document.querySelector('.previous');
+let item = document.querySelector('.item');
+
+
+//выбор цвета + текущий/предыдущий
+const paint = document.querySelector('.colors');
+paint.addEventListener("click" , function(e){
+    if (e.srcElement.children[0]) {
+        state.prevColor = state.currentColor;
+        state.currentColor = e.srcElement.children[0].className;
+                
+        current.style.backgroundColor = state.currentColor;
+        previous.style.backgroundColor = state.prevColor;
+
+    } else {
+        state.currentColor = e.srcElement.className; 
+    }
+})
+
+
+//если включен пенсил и выбран цвет, то сделав точку закрашивается квадратик
+//если включена пипетка, то нажав на квадратик меняется текущий и предыдущий цвет
+const bigSquare = document.querySelector('.canvas ');
+bigSquare.addEventListener('click', function(e) {
+    if (state.currentTool === 'pencil') {
+    e.srcElement.style.backgroundColor = state.currentColor;
+} else if (state.currentTool === 'pipetka') {
+    state.prevColor = state.currentColor;
+    state.currentColor = e.srcElement.style.backgroundColor;
+
+    current.style.backgroundColor = state.currentColor;
+    previous.style.backgroundColor = state.prevColor;
+    console.log(state.currentTool);
+} else if(state.currentTool === 'shape') {
+    e.srcElement.classList.toggle('item_2');
+}
+
+} )
+
+//включение/выключение пенсил
+const pencil = document.querySelector('.pencil'); 
+pencil.addEventListener('click', function() {
+    state.currentTool = 'pencil';
+})
+
+const pipetka = document.querySelector('.pipetka');
+pipetka.addEventListener('click', function() {
+state.currentTool = 'pipetka';
+})
+
+const shape = document.querySelector('.shape');
+shape.addEventListener('click', function(){
+    state.currentTool = 'shape';
+})
