307cd004714b1e39e4a492c87aec882c3c4e13fe
user <anyakorytko@mail.ru>
2020-08-27 00:54:52 +0300

    "no position. yes border"

--- script.js
+++ script.js
@@ -9,16 +9,21 @@ let current = document.querySelector('.current');
 let previous = document.querySelector('.previous');
 let item = document.querySelector('.item');
 let item_2 = document.querySelector('#item_2');
+let current_1 = document.querySelector('#current_1');
 
 //выбор цвета + текущий/предыдущий
 const paint = document.querySelector('.colors');
 paint.addEventListener("click" , function(e){
+    current_1.style.border = '5px double coral';
     if (e.srcElement.children[0]) {
         state.prevColor = state.currentColor;
         state.currentColor = e.srcElement.children[0].className;
-                
+        
+        
         current.style.backgroundColor = state.currentColor;
         previous.style.backgroundColor = state.prevColor;
+
+        //e.srcElement.style.border = '5px double coral';
 
     } else {
         state.currentColor = e.srcElement.className; 
@@ -49,14 +54,26 @@ bigSquare.addEventListener('click', function(e) {
 const pencil = document.querySelector('.pencil'); 
 pencil.addEventListener('click', function() {
     state.currentTool = 'pencil';
+    pencil.style.border = '5px double coral';
+    pipetka.style.border = 'none';
+    shape.style.border = 'none';
+    paint.style.border = 'none';
 })
 
 const pipetka = document.querySelector('.pipetka');
 pipetka.addEventListener('click', function() {
-state.currentTool = 'pipetka';
+    state.currentTool = 'pipetka';
+    pipetka.style.border = '5px double coral';
+    pencil.style.border = 'none';
+    shape.style.border = 'none';
+    paint.style.border = 'none';
 })
 
 const shape = document.querySelector('.shape');
 shape.addEventListener('click', function(){
     state.currentTool = 'shape';
+    shape.style.border = '5px double coral';
+    pencil.style.border = 'none';
+    pipetka.style.border = 'none';
+    paint.style.border = 'none';
 })
